6e6de3f6a26b6b5e1dd8104f93322fff64d6ff9b
ultraViz <you@example.com>
2022-05-28 23:42:11 +0200

    Light Dark Mode Toggle

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
-const videoElement = document.getElementById('video')
-const btn = document.getElementById('button')
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
 
+//Dark Mode Styles
+function darkMode(){
+  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  toggleIcon.children[0].textContent = 'Dark Mode';
+  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+  image1.src = 'img/undraw_proud_coder_dark.svg';
+  image2.src = 'img/undraw_conceptual_idea_dark.svg';
+  image3.src = 'img/undraw_feeling_proud_dark.svg';
+}
 
-// Prompt to select media stream, pass to video element then play
-
-async function selectMediaStream(){
-  try{
-    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
-
-    videoElement.srcObject =  mediaStream;
-    videoElement.onloadedmetadata = () => {
-      videoElement.play();
-    }
-  }catch(error){
-    // Catch errors
-    
+//Light Mode Styles
+function lightMode(){
+  nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  toggleIcon.children[0].textContent = 'Light Mode';
+  toggleIcon.children[1].classList.remove('fa-moon');
+  toggleIcon.children[1].classList.add('fa-sun');
+  image1.src = 'img/undraw_proud_coder_light.svg';
+  image2.src = 'img/undraw_conceptual_idea_light.svg';
+  image3.src = 'img/undraw_feeling_proud_light.svg';
+}
+//lightSwitch Theme Dynemicly
+function switchTheme(event){
+  if(event.target.checked){
+    document.documentElement.setAttribute('data-theme', 'dark');
+    darkMode();
+  }else{
+    document.documentElement.setAttribute('data-theme', 'light')
+    lightMode();
   }
 }
 
-btn.addEventListener('click', async ()=>{
-  //  Disable button
-  btn.disabled = true;
-  // Start P in P
-  await videoElement.requestPictureInPicture();
-  // Reset button
-  btn.disabled =  false;
-});
 
-
-
-//on Load
-selectMediaStream();
-
+//Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
